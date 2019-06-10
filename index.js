2d71a6b7aba30bc0494af02eebf40f4e7b58e4e3
Les Arrow <les@larrowtech.com>
2019-06-10 13:56:04 -0700

    assignment 2

--- index.js
+++ index.js
@@ -4,6 +4,17 @@
 function displayResults(jsonObj) {
 
     console.log(jsonObj);
+
+    /* Build the HTML string for the images and 
+       Replace the random dog container div with contents from the jsonObj */
+
+    let images_html = `<h1>Here are your ${jsonObj.message.length} random dog pics:</h1>`;
+
+    for (let i=0; i<jsonObj.message.length; i++)
+        images_html += `<img class="dogpic" src="${jsonObj.message[i]}">`;
+
+    $('.random-dog-container').html(images_html);
+
 }
 
 function handleDogApp() {
